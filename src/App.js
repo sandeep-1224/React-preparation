7e613ef2d1a835286ea2468c4146f3c4b6f68b7d
sandeep-1224 <sandeepsharma8922@gmail.com>
2023-09-25 16:52:14 +0530

    updated deleted file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import Footer from './Footer';
-import SideBar from './SideBar';
-import TopBar from './TopBar';
-
 function App() {
   return (
     <div>
-      <TopBar/>
-      <SideBar/>
-      {/* <Footer/> */}
+      <h1>Hello React</h1>
     </div>
   );
 }
